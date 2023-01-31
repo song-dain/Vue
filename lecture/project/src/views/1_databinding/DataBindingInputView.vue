2536ff35cae43f95a5557470b9b7e89da953ae3f
송다인 <inhere31@gmail.com>
2023-01-31 18:09:18 +0900

    project 2/4

---
+++ lecture/project/src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" />
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'dainsong'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ lecture/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hellow {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'Welcome 개발자의 품격'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
